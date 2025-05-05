193af92ecbc88aa2de19f95b028c7bed1b2af09b
NguyenAnh20055 <161116528+NguyenAnh20055@users.noreply.github.com>
2025-05-05 07:40:09 +0700

    Add files via upload

---
+++ LAB2/Controllers/TodoController1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LAB2.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
