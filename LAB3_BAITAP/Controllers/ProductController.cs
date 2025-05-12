ca0c1b246c3a3eb6d79aff52cdcc69e4aa29ffe0
NguyenAnh20055 <161116528+NguyenAnh20055@users.noreply.github.com>
2025-05-12 07:42:20 +0700

    Add files via upload

---
+++ LAB3_BAITAP/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using LAB3_BAITAP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAB3_BAITAP.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        static List<Product> products = new List<Product>()
+             {
+             new Product() {Id=1,Name="Son",Price=200000},
+             new Product() {Id=2,Name="Son",Price= 15000},
+             new Product() {Id=3,Name="Sữa",Price=50000},
+             new Product() {Id=4,Name="Kẹo",Price= 33000},
+             new Product() {Id=5,Name="Bim Bim",Price= 5000}
+             };
+        public IActionResult Details()
+        {
+            return View(products);
+        }
+    }
+}
---
+++ LAB3_BAITAP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LAB3_BAITAP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
