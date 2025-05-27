41a74a03b82ccdf3e4422cda1a4c4f13b809f069
NguyenAnh20055 <161116528+NguyenAnh20055@users.noreply.github.com>
2025-05-28 00:09:10 +0700

    Add files via upload

---
+++ LAB4/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using LAB4.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace LAB4.Controllers
+{
+    public class UserController : Controller
+    {
+        static List<User> users = new List<User>()
+ {
+ new User() {id="1",username="aaa",password="1",phone="111",email="111"},
+ new User() {id="1",username="aaa",password="1",phone="111",email="111"},
+ new User() {id="1",username="aaa",password="1",phone="111",email="111"},
+ new User() {id="1",username="aaa",password="1",phone="111",email="111"},
+ new User() {id="1",username="aaa",password="1",phone="111",email="111"},
+ };
+        public IActionResult Index()
+        {
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public IActionResult Add(User user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    users.Add(user);
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+            return View(User);
+
+        }
+        
+    }
+}
